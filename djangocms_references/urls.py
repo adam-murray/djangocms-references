1704e10596d1ddb15159db1b3aeebad7b029539f
Adam Murray <adam-murray@hotmail.co.uk>
2022-03-18 13:51:32 +0000

    Fix: URL registered namespace

--- djangocms_references/urls.py
+++ djangocms_references/urls.py
@@ -4,11 +4,10 @@ from django.urls import re_path
 from .views import ReferencesView
 
 
-app_name = "djangocms_references"
 urlpatterns = [
     re_path(
         r"^references/(?P<content_type_id>\d+)/(?P<object_id>\d+)/$",
         staff_member_required(ReferencesView.as_view()),
-        name="references-index",
+        namespace="references-index",
     )
 ]
